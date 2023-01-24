b524a2bafaae2a8875be4cf7b9351b79e4ad169a
Chandan Kumar <chandankumar49935@gmail.com>
2023-01-25 00:50:29 +0530

    09. Palindrome Number

---
+++ src/Solution_09.java
@@ -0,0 +1,22 @@
+/**
+ * 09. Palindrome Number
+ * Difficulty : Easy
+ */
+
+public class Solution_09 {
+    public boolean isPalindrome(int x) {
+        int sum = 0;
+        int temp = x;
+        while(x > 0) {
+            int rem = x % 10;
+            sum = (sum * 10) + rem;
+            x = x / 10;
+        }
+        if(sum == temp) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
