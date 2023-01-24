b5325ad94844e48a7def5ac08985993b0c6b9f9f
Chandan Kumar <chandankumar49935@gmail.com>
2023-01-25 00:49:39 +0530

    08. String to Integer (atoi)

---
+++ src/Solution_08.java
@@ -0,0 +1,40 @@
+/**
+ * 08. String to Integer (atoi)
+ * Difficulty : Medium
+ */
+
+public class Solution_08 {
+    public int myAtoi(String s) {
+        if(s == null) {
+            return 0;
+        }
+        s = s.trim();
+        if(s.length() == 0) {
+            return 0;
+        }
+        int sign = +1;
+        long res = 0;
+        if(s.charAt(0) == '-') {
+            sign = -1;
+        }
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+        int i = (s.charAt(0) == '+' || s.charAt(0) == '-') ? 1 : 0;
+
+        while(i < s.length()) {
+            if(s.charAt(i) == ' ' || !Character.isDigit(s.charAt(i))) {
+                break;
+            }
+            res = res * 10 + s.charAt(i) - '0';
+            if(sign == -1 && -1*res < min) {
+                return min;
+            }
+            if(sign == 1 && res > max) {
+                return max;
+            }
+            i++;
+        }
+        return (int)(sign*res);
+    }
+}
+
