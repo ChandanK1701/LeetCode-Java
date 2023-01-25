d1fdfc3148616f5d8bbd46ba66634de9c56898f7
Chandan Kumar <chandankumar49935@gmail.com>
2023-01-26 00:00:30 +0530

    0053. Maximum Subarray

---
+++ src/Solution_0053.java
@@ -0,0 +1,19 @@
+/**
+ * 0053. Maximum Subarray
+ * Difficulty : Medium
+ */
+
+public class Solution_0053 {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxSum = nums[0];
+        for(int i=0; i<nums.length; i++) {
+            sum = sum + nums[i];
+            maxSum = Math.max(maxSum, sum);
+            if(sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
