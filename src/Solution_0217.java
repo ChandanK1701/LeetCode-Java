9cd69c80da6b516314ae3941518bf0e54ef29697
Chandan Kumar <chandankumar49935@gmail.com>
2023-01-26 19:34:47 +0530

    0217. Contains Duplicate

---
+++ src/Solution_0217.java
@@ -0,0 +1,16 @@
+/**
+ * 0217. Contains Duplicate
+ * Difficulty : Easy
+ */
+
+public class Solution_0217 {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length-1; i++) {
+            if(nums[i] == nums[i+1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
