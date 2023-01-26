fcb7ed7fbbfdcd2f0349a6c795136248f5d64711
Chandan Kumar <chandankumar49935@gmail.com>
2023-01-26 19:19:29 +0530

    0144. Binary Tree Preorder Traversal

---
+++ src/Solution_0144.java
@@ -0,0 +1,44 @@
+/**
+ * 0144. Binary Tree Preorder Traversal
+ * Difficulty : Easy
+ */
+
+/****************************************/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution_0144 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> preorder = new ArrayList<Integer>();
+        if(root == null) {
+            return preorder;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while(!stack.isEmpty()) {
+            root = stack.pop();
+            preorder.add(root.val);
+            if(root.right != null) {
+                stack.push(root.right);
+            }
+            if(root.left != null) {
+                stack.push(root.left);
+            }
+        }
+        return preorder;
+    }
+}
