10f4421021034582de2339950f399ced10bdb091
Chandan Kumar <chandankumar49935@gmail.com>
2023-01-26 20:16:36 +0530

    0912. Sort an Array

---
+++ src/Solution_0912.java
@@ -0,0 +1,11 @@
+/**
+ * 0912. Sort an Array
+ * Difficulty : Medium
+ */
+
+public class Solution_0912 {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
