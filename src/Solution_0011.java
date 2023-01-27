95b91fc496114f541d283e7304842fc68e5f2030
Chandan Kumar <chandankumar49935@gmail.com>
2023-01-27 15:09:39 +0530

    0011. Container With Most Water

---
+++ src/Solution_0011.java
@@ -0,0 +1,27 @@
+/**
+ * 0011. Container With Most Water
+ * Difficulty : Medium
+ */
+
+public class Solution_0011 {
+    public int maxArea(int[] height) {
+        int start = 0;
+        int end = height.length-1;
+        int area;
+        int maxArea = 0;
+
+        while(start < end) {
+            area = Math.min(height[start], height[end]) * (end - start);
+            if(area > maxArea) {
+                maxArea = area;
+            }
+            if(Math.min(height[start], height[end]) == height[start]) {
+                start++;
+            }
+            else {
+                end--;
+            }
+        }
+        return maxArea;
+    }
+}
