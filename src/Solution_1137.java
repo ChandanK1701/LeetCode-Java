a562be07c4a40adf042efda42efd6e57d3749e59
Chandan Kumar <chandankumar49935@gmail.com>
2023-01-30 11:48:03 +0530

    1137. N-th Tribonacci Number

---
+++ src/Solution_1137.java
@@ -0,0 +1,17 @@
+/**
+ * 1137. N-th Tribonacci Number
+ * Difficulty : Easy
+ */
+
+public class Solution_1137 {
+    public int tribonacci(int n) {
+        int[] dp = new int[38];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i=3; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        return dp[n];
+    }
+}
