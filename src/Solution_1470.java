c3fff94d113e2ed6cb4c42874b64107f4564cad9
Chandan Kumar <chandankumar49935@gmail.com>
2023-02-06 10:56:55 +0530

    1470. Shuffle the Array

---
+++ src/Solution_1470.java
@@ -0,0 +1,19 @@
+/**
+ * 1470. Shuffle the Array
+ * Difficulty : Easy
+ */
+
+public class Solution_1470 {
+    public int[] shuffle(int[] nums, int n) {
+        int[] res = new int[2*n];
+        for(int i=0; i<2*n; i++) {
+            if(i % 2 == 0) {
+                res[i] = nums[i / 2];
+            }
+            else {
+                res[i] = nums[n + i / 2];
+            }
+        }
+        return res;
+    }
+}
